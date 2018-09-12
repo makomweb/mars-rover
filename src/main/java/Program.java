f283306c678e08fe3b06133116cdb065bfb1c4c5
Martin Komischke <martin.komischke@outlook.com>
2018-09-12 13:31:38 +0200

    enable program being executed as Java application

---
+++ src/main/java/Program.java
@@ -0,0 +1,12 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		AClass obj = new AClass();
+		
+		int sum = obj.sum(23, 34);
+		
+		System.out.println("Sum is: " + sum);
+	}
+}
