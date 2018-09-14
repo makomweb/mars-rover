48b3030378b456858bb1e3b3f2794002c4d9d642
Martin Komischke <martin.komischke@outlook.com>
2018-09-14 17:09:38 +0200

    improve command line interface

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,22 +1,51 @@
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Plateau p = new Plateau(5, 5);
+		Scanner scanner = new Scanner(System.in);
 		
-		{
-			Rover rover = new Rover("One");
-			rover.dropRover(p, "1 2 N");
-			rover.processInstructions("LMLMLMLMM");
-			System.out.println("Rover report - " + rover.toString());
-		}
+		System.out.print("Enter dimensions of the plateau (in the form x y):");
+		String dimensions = scanner.nextLine();
+		
+		Plateau p = createPlateauFromDimensions(dimensions);
+		
+		int i = 0;
+		while (true) {
 
-		{
-			Rover rover = new Rover("Two");
-			rover.dropRover(p, "3 3 E");
-			rover.processInstructions("MMRMMRMRRM");
-			System.out.println("Rover report - " + rover.toString());
+			i++;
+			
+			String name = "Rover " + i;
+			
+			System.out.print("Enter drop information for " + name + " (in the form x y h):");
+			String dropInfo = scanner.nextLine();
+						
+			Rover r = dropRover(name, p, dropInfo);
+			System.out.println("Report: " + r.reportPosition());
+
+			System.out.print("Enter instructions for " + name + " (in the form LMRMMMLMM):");
+			String instructions = scanner.nextLine();
+
+			r.processInstructions(instructions);
+			System.out.println("Report: " + r.reportPosition());
 		}
+	}
+	
+	private static Rover dropRover(String roverId, Plateau p, String dropInfo) {		
+		
+		Rover r = new Rover(roverId);
+		r.dropRover(p,  dropInfo);
+		
+		return r;
+	}
+
+	private static Plateau createPlateauFromDimensions(String dimensions) {
+		
+		String[] parts = dimensions.split(" ");
+		int dimX = Integer.parseInt(parts[0]);
+		int dimY = Integer.parseInt(parts[1]);
+		
+		return new Plateau(dimX, dimY);
 	}
 }
