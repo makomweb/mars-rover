1644fb26a59d5cfab9088ef13fcfe921d215cb23
Martin Komischke <martin.komischke@outlook.com>
2018-10-02 15:06:53 +0200

    remove another obsolete class

--- src/test/java/FailingTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-public class FailingTest {
-   @Test
-   public void testThatFails() {
-       assertTrue("This should fail", false);
-   }
-}
